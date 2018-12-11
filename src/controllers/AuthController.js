88f86e16f3d1b63c7227c9c262685e76734155b4
Wilson Filho <will.filho27@gmail.com>
2018-12-11 18:03:56 -0300

    add endpoint register

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,10 +3,14 @@ const User = require('../models/User')
 module.exports = {
     async register(req, res) {
         try {
+            const { email } = req.body
+            if (await User.findOne({ email }))
+            return res.status(400).send({ error: 'User already exists'})
             const user = await User.create(req.body)
+            user.password = undefined
             return res.send({ user })
         } catch (err) {
-            return res.send(400, {error: 'Registration failed'})
+            return res.status(400).send({error: 'Registration failed'})
         }
     }
 }
